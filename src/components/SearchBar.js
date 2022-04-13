7970e1dbc493da70850ad649d700a70425ba5bbb
Hemraj K Shettigar <hkshettigar@yahoo.com>
2022-04-13 15:55:50 +0530

    updated label

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './imageList.css';
 
 class SearchBar extends React.Component {
   state = { term: '' };
@@ -9,20 +10,22 @@ class SearchBar extends React.Component {
   };
   render() {
     return (
-      <div className="ui segment">
-        <form
-          onSubmit={(event) => this.onFormSubmit(event)}
-          className="ui form"
-        >
-          <div className="field">
-            <label>Image Search</label>
-            <input
-              type="text"
-              value={this.state.term}
-              onChange={(e) => this.setState({ term: e.target.value })}
-            />
-          </div>
-        </form>
+      <div className="ui segment ">
+        <div>
+          <form
+            onSubmit={(event) => this.onFormSubmit(event)}
+            className="ui form"
+          >
+            <div className="field">
+              <label>Image Search</label>
+              <input
+                type="text"
+                value={this.state.term}
+                onChange={(e) => this.setState({ term: e.target.value })}
+              />
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
